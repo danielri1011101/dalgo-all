081f1922e5fb8a27ca62792db0b65f87b72a54ef
Daniel R Barrero R <barreroroserodr@gmail.com>
2025-08-07 12:45:52 -0500

    Wrote Fibonacci examples.

---
+++ repo202520/Aula02.java
@@ -0,0 +1,33 @@
+public class Aula02 {
+
+	/**
+	 * Returns the n-th Fibonacci number, computed iteratively
+	 * In Java it is O(n) for n at most 45
+	 * @param n positive integer
+	 */
+	public static int fibIter(int n) {
+		int a= 0;
+		int b= 1; // larger number
+		for (int i= 0; i < n; i++) {
+			int temp= b;
+			b+= a;
+			a= temp;
+		}
+		return b;
+	}
+
+	/**
+	 * Computes Fibonacci numbers recursively
+	 * What is its time and space complexity?
+	 */
+	public static int fibRec(int n) {
+		if (n < 2) {
+			return n;
+		}
+		return fibRec(n-1) + fibRec(n-2);
+	}
+
+	public static void main(String[] args) {
+		System.out.println(fibRec(8));
+	}
+}
