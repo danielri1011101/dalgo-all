b77755bf3acd9f05fa94f0fc33864a574d5b7977
Daniel R Barrero R <barreroroserodr@gmail.com>
2025-08-29 18:11:48 -0500

    Writing greedy make change.

--- repo202520/diapositivas/semana05/Aula08.java
+++ repo202520/diapositivas/semana05/Aula08.java
@@ -1,4 +1,13 @@
 public class Aula08 {
+
+	/**
+	 * Greedy algorithm for making change. When is it incorrect or sub-optimal?
+	 * @param denoms The possible values of our currency. Sorted decreasingly.
+	 * @param v The value to which the coins must add up.
+	 */
+	int[] gMakeChange(int[] denoms, int v) {
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Hola dalgo!");
 	}
