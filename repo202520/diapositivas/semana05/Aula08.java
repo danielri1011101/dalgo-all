a557a709c1607e9635165d750897bb7bc053a853
Daniel R Barrero R <barreroroserodr@gmail.com>
2025-08-30 15:20:44 -0500

    Writing slides for week 5, organized homeworks folder.

--- repo202520/diapositivas/semana05/Aula08.java
+++ repo202520/diapositivas/semana05/Aula08.java
@@ -6,9 +6,40 @@ public class Aula08 {
 	 * @param v The value to which the coins must add up.
 	 */
 	int[] gMakeChange(int[] denoms, int v) {
+		int n= denoms.length;
+		int[] ans= new int[n];
+		int i= 0;
+		while (i < n && v > 0) {
+			int d= denoms[i];
+			while (d > v && i < n-1) {
+				i++;
+				d= denoms[i];
+			}
+			while (d <= v) {
+				v-= d;
+				ans[i]++;
+			}
+			i++;
+		}
+		return ans;
+	}
+
+	public static void printArray(int[] arr) {
+		System.out.print("{");
+		int n= arr.length;
+		for (int i= 0; i < n-1; i++) {
+			System.out.print(arr[i] + ", ");
+		}
+		System.out.println(arr[n-1] + "}");
 	}
 
 	public static void main(String[] args) {
+		Aula08 au= new Aula08();
+		int[] denoms= {5,4,2,1};
+		int[] values= {8, 11, 18};
+		for (int v : values) {
+			printArray(au.gMakeChange(denoms, v));
+		}
 		System.out.println("Hola dalgo!");
 	}
 }
