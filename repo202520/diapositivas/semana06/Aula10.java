e07188c39dd4623a957f2788afa9e8a0ea2981a0
Daniel R Barrero R <barreroroserodr@gmail.com>
2025-09-08 06:42:18 -0500

    Code for full knapsack compiles but there's a bug: it picks all items except the first.

--- repo202520/diapositivas/semana06/Aula10.java
+++ repo202520/diapositivas/semana06/Aula10.java
@@ -10,28 +10,65 @@ public class Aula10 {
 	 */
 	boolean[] knapsack(int[] ws, int[] vs, int maxW) {
 		int n= ws.length;
-		boolean[] its= new boolean[n];
 
 		// memo[i][j] == max value for items 0..i and capacity j.
 		int[][] memo= new int[n][maxW+1];
-		for (int i= 0; i < n; i++) {
+
+		// its[i] <=> i-th item belongs in the knapsack.
+		boolean[][] mits= new boolean[maxW+1][n];
+
+		for (int j= 0; j <= maxW; j++) {
+			int v0= vs[0];
+			int w0= ws[0];
+			if (j >= w0) {
+				memo[0][j]= v0;
+				mits[j][0]= true;
+			}
+		}
+		for (int i= 1; i < n; i++) {
 			int wi= ws[i];
 			int vi= vs[i];
 			for (int j= 0; j <= maxW; j++) {
 				int r= j-wi;
-				if (j < 0 && i > 0) {
-					memo[i][j]= memo[i-1][j];
+				if (r >= 0 && vi + memo[i-1][j-wi] > memo[i-1][j]) {
+					memo[i][j]= vi + memo[i-1][j-wi];
+					mits[j]= mits[j-wi];
+					mits[j][i]= true;
 				}
-				if (j >= 0 && i == 0) {
-					memo[i][j]= vi;
-					its[i]= true;
+				else {
+					memo[i][j]= memo[i-1][j];
 				}
 			}
 		}
+		boolean[] its= mits[maxW];
 		return its;
 	}
 
+	void printKnapsack(int[] ws, int[] vs, int maxW) {
+		boolean[] its= knapsack(ws, vs, maxW);
+		int n= its.length;
+
+		int v= 0;
+		System.out.println("The selected items are:");
+		for (int u= 0; u < n; u++) {
+			if (its[u]) {
+				System.out.println("(" + ws[u] + ", " + vs[u] +
+						")");
+				v+= vs[u];
+			}
+		}
+
+		System.out.println();
+		System.out.println("With maximum value");
+		System.out.println(v);
+		System.out.println("for weight limit " + maxW);
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Aula10");
+		int[] ws= {1, 2, 3, 5, 6, 7};
+		int[] vs= {1, 6, 7, 18, 22, 28};
+		int maxW= 11;
+		Aula10 au= new Aula10();
+		au.printKnapsack(ws,vs,maxW);
 	}
 }
