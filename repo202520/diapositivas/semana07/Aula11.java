3003c3bc8c052c73062b58688656e749fad2b98e
Daniel R Barrero R <barreroroserodr@gmail.com>
2025-09-15 11:41:18 -0500

    Slides for week 7.

--- repo202520/diapositivas/semana07/Aula11.java
+++ repo202520/diapositivas/semana07/Aula11.java
@@ -42,6 +42,7 @@ public class Aula11 {
 		/**
 		 * @return Whether or not the graph satisfies the mathematical
 		 * definition
+		 * @notice Full validity requires checking it is not a multigraph
 		 */
 		boolean getValidity() {
 			int n= this.getNnodes();
@@ -55,7 +56,21 @@ public class Aula11 {
 	// List graphs
 	//
 
+	// Singly linked list helper class
+	class SLList {
+		LLNode head;
+	}
+
+	// Linked list node helper class
+	class LLNode {
+		int nodeVal;
+		LLNode next;
+	}
+
 	class LisGraph {
+
+		// The elements of the array are the lists of neighbors
+		SLList[] nodes;
 	}
 
 	//
@@ -65,16 +80,25 @@ public class Aula11 {
 	public static void main(String[] args) {
 		System.out.println("Aula11");
 
+		// Controlling instance
+		Aula11 au= new Aula11();
+
 		// Short path graph
 		int[] spgNodes= {1, 2, 7};
-		boolean[] node0Conns= {};
-		boolean[] node1Conns= {};
-		boolean[] node2Conns= {};
+		boolean[] node0Conns= {false, true, false};
+		boolean[] node1Conns= {false, false, true};
+		boolean[] node2Conns= {false, false, false};
+		boolean[][] spgAdjMat= {node0Conns, node1Conns, node2Conns};
+		AdjGraph spg= au.new AdjGraph(spgNodes, spgAdjMat);
+		System.out.println(spg.getValidity());
 
-		// Digraph where node [2]1 has out-degree two
+		// Directed graph where node [2]1 has out-degree two
 		int[] digNodes= {1, 2, 7};
-		boolean[] node0Conns1= {};
-		boolean[] node1Conns1= {};
-		boolean[] node2Conns1= {};
+		boolean[] node0Conns1= {false, false, false};
+		boolean[] node1Conns1= {true, false, true};
+		boolean[] node2Conns1= {false, false, false};
+		boolean[][] digAdjMat= {node0Conns1, node1Conns1, node2Conns1};
+		AdjGraph dig= au.new AdjGraph(digNodes, digAdjMat);
+		System.out.println(dig.getValidity());
 	}
 }
