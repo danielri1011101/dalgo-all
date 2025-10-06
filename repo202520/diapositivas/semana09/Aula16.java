2e6d309427d3d06b8cd459c5789b16763140366b
Daniel R Barrero R <barreroroserodr@gmail.com>
2025-10-06 09:56:23 -0500

    Floyd-Warshall for monday lecture, week 9.

---
+++ repo202520/diapositivas/semana09/Aula16.java
@@ -0,0 +1,12 @@
+public class Aula16 {
+
+	/**
+	 * Directed graph with lengths
+	 */
+	static class Graph {
+		int[][] pLens; // Square matrix with positive entries
+	}
+	public static void main(String[] args) {
+		System.out.println("Aula16");
+	}
+}
