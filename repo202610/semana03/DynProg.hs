fc9f6660f03429f506087026d0c22a33c5df7c8b
Daniel R Barrero R <barreroroserodr@gmail.com>
2026-02-07 19:30:02 -0500

    Finished knapsack in haskell. Statement of homework 2.

--- repo202610/semana03/DynProg.hs
+++ repo202610/semana03/DynProg.hs
@@ -15,10 +15,51 @@ badKnapsack ((wx, vx):wvs) maxW
 -- Helper types and functions for a better version of Knapsack
 
 type MMap= Map.Map
+type KItem= (Int, Int)
 
-mlookup :: String -> MMap String a -> Maybe a
+mlookup :: Int -> MMap Int a -> Maybe a
 mlookup= Map.lookup
 
-type KItem= (Int, Int, String)
+knapsack :: Int -> [KItem] -> MMap Int Int
+knapsack _ []= Map.fromList []
+knapsack maxW (y:ys)
+  | maxW <= 0= Map.fromList []
+  | otherwise=
+      let base= knapsackBase maxW y
+      in snd (foldl go (maxW, base) ys)
+      where
+    go (weight, prev_row) item=
+      (weight, knapsackRow weight item prev_row)
 
-type KMemo= (MMap String Bool, MMap String Int)
+knapsackRow :: Int -> KItem -> MMap Int Int -> MMap Int Int
+knapsackRow maxW (cw, cv) prev_row= Map.fromList (krw maxW (cw, cv) prev_row)
+  where
+krw wt _ _
+  | wt <= 0= []
+krw wt (cw, cv) prev_row
+  | wt < cw=
+      let prev_sol1= extract 0 (mlookup wt prev_row)
+      in (wt, prev_sol1) : krw (wt-1) (cw, cv) prev_row
+  | otherwise=
+      let prev_sol1= extract 0 (mlookup wt prev_row)
+          prev_sol2= extract 0 (mlookup (wt-cw) prev_row)
+      in (wt, max prev_sol1 (prev_sol2 + cv)) : krw (wt-1) (cw, cv) prev_row
+
+knapsackBase :: Int -> KItem -> MMap Int Int
+knapsackBase maxW (w, v)= Map.fromList (kbs maxW (w, v))
+  where
+kbs maxW (w, v)
+  | maxW <= 0= [(0,0)]
+  | w > maxW= (maxW, 0) : kbs (maxW-1) (w,v)
+  | otherwise= (maxW, v) : kbs (maxW-1) (w,v)
+
+extract :: a -> Maybe a -> a
+extract a0 Nothing= a0
+extract _ (Just a)= a
+
+-- Example from the book
+exl_items= [(1,1), (2,6), (3,7), (5,18), (6,22), (7,28)]
+
+exl_sol_map= knapsack 11 exl_items
+
+exl_sol_value= extract 0 (mlookup 11 exl_sol_map)
