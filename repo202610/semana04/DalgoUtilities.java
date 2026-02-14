a59772283773b907feb7d31adb5ee4ad7940b4c1
Daniel R Barrero R <barreroroserodr@gmail.com>
2026-02-14 18:45:23 -0500

    Elementary queues drafted, must test.

--- repo202610/semana04/DalgoUtilities.java
+++ repo202610/semana04/DalgoUtilities.java
@@ -4,10 +4,42 @@ public class DalgoUtilities {
 		QNode tail;
 
 		public QNode dequeue() {
-			return null;
+			QNode ans= this.head;
+			if (ans.succ == null) {
+				this.head= null;
+				this.tail= null;
+				return ans;
+			}
+			QNode nh= ans.succ;
+			ans.succ= null;
+			nh.pred= null;
+			this.head= nh;
+			return ans;
 		}
 
 		public void enqueue(QNode node) {
+			QNode pt= this.tail;
+			if (pt == null) {
+				this.head= node;
+				this.tail= node;
+				return;
+			}
+			pt.succ= node;
+			node.pred= pt;
+			node.succ= null;
+			this.tail= node;
+		}
+
+		public void printMe() {
+			QNode current= this.head;
+			int t= current.ntag;
+			while (current.succ != null) {
+				System.out.print(t);
+				System.out.print(" <-> ");
+				current= current.succ;
+				t= current.ntag;
+			}
+			System.out.println(t);
 		}
 	}
 
