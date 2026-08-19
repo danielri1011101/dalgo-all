a1c8f53b3b92660b9b041633616662e3d475ba6a
Daniel R Barrero R <barreroroserodr@gmail.com>
2026-08-18 22:34:45 -0500

    semana 3 slides and tarea 2

---
+++ repo202620/semana03/Aula09.java
@@ -0,0 +1,214 @@
+public class Aula09 {
+
+	/**
+	 * 250904
+	 * For a memory function aproach to knapsack, have the memory function
+	 * check and store the subproblems of maximum value with limiting weight
+	 * between min{wi} and maxW, checked and filled according to need
+	 *
+	 * 250905
+	 * Table filling for matrix bracketing as in CYK parsing algorithm
+	 */
+
+	//
+	// Knapsack
+	//
+
+	/**
+	 * Greedy algorithm for solving the knapsack problem
+	 * @param ws Weights
+	 * @param vs Values
+	 * Weights and values arrays are assumed of equal length so that they
+	 * can be read as an array of weight,value pairs.
+	 * @return ans The maximum value attainable by this greedy processing.
+	 */
+	int gKnapsack(int[] ws, int[] vs, int maxW) {
+		int ans= 0;
+		int r= maxW;
+		int j= 0;
+		int n= ws.length;
+		while (r > 0 && j < n) {
+			int wj= ws[j];
+			int vj= vs[j];
+			if (wj < r) {
+				r-= wj;
+				ans+= vj;
+			}
+			j++;
+		}
+		return ans;
+	}
+
+	/**
+	 * Dynamic programming algorithm for the Knapsack problem
+	 * @param ws The array of weights
+	 * @param vs The corresponding array of values
+	 * @assert ws, vs must be of equal length
+	 */
+	int buKnapsack(int[] ws, int[] vs, int maxW) {
+		int n= ws.length;
+		int[][] subs= new int[n+1][maxW+1];
+		for (int i= 1; i <= n; i++) {
+			int wi= ws[i-1];
+			int vi= vs[i-1];
+			for (int j= 1; j <= maxW; j++) {
+				if (wi > j) {
+					subs[i][j]= subs[i-1][j];
+				}
+				subs[i][j]= Math.max(subs[i-1][j], vi +
+						subs[i-1][j-wi]);
+			}
+		}
+		return subs[n][maxW];
+	}
+
+	/**
+	 * Solution to the Knapsack problem using a memory function.
+	 */
+	int funKnapsack(int[] ws, int[] vs, int maxW) {
+
+		// Max value table. w-th entry is max value with weight limit w.
+		int[] mvt= new int[maxW+1];
+		return fKnapsack(ws, vs, mvt, maxW);
+	}
+
+	/**
+	 * @param ws Weights. Positive
+	 * @param vs Values. Positive.
+	 * @param subs Matrix of subproblems' solutions
+	 * @param j Item index, in 0..n-1
+	 * @param c The capacity of the Knapsack
+	 */
+	int tdKnapsack(int[] ws, int[] vs, int[][] subs, int j, int c) {
+		if (subs[j][c] > 0) {
+			return subs[j][c]; // Return if solution already computed
+		}
+		int cuv= vs[j]; // Value of current Item
+		int cuw= ws[j]; // Weight of current Item
+		if (cuw > c) {
+			return tdKnapsack(ws, vs, j-1, c); // Current item is too heavy
+		}
+		int prev1= 0;
+		if (subs[j-1][c-cuw] > 0) {
+			prev1= subs[j-1][c-cuw];
+		}
+		else {
+			prev1= tdKnapsack(ws, vs, j-1, c-cuw);
+		}
+		int sol1= prev1 + cuv; // Best solution INCLUDING current item
+		int prev2= 0;
+		if (subs[j-1][c] > 0) {
+			prev2= subs[j-1][c];
+		}
+		else {
+			prev2= tdKnapsack(ws, vs, j-1, c);
+		}
+		int sol2= prev2; // Best solution WITHOUT current item
+		int sol= Math.max(sol1, sol2);
+		subs[j][c]= sol; // Update solutions table
+		return sol;
+	}
+
+	/**
+	 * @return xs[0..k-1]
+	 * @assert k \geq 0
+	 */
+	private static int[] subArr(int[] xs, int k) {
+		int[] ans= new int[k];
+		for (int i= 0; i < k; i++) {
+			ans[i]= xs[i];
+		}
+		return ans;
+	}
+
+	//
+	// Matrix bracketing
+	//
+
+	/**
+	 * @param rs Array of factors' ranks, amount of factors is one less
+	 * mi has dimensions rs[i-1] x rs[i] for i \in [1..n]
+	 * @return Number of multiplications required with the optimal bracketing
+	 */
+	int dpMb(int[] rs) {
+		int n= rs.length;
+
+		// memo[i][j] Solution to subproblem mi*...*mj
+		int[][] memo= new int[n][n];
+		int s= 1;
+		for (int i= 1; i < n-s; i++) {
+			memo[i][i+s]= rs[i-1]*rs[i]*rs[i+1];
+		}
+		s++;
+		while (s < n-1) {
+			for (int i= 1; i+s < n; i++) {
+				int mults= Integer.MAX_VALUE;
+				for (int k= 1; k < s; k++) {
+					int msl= memo[i][i+k];
+					int msr= memo[i+k+1][i+s];
+					int oms= rs[i-1]*rs[i+k]*rs[i+s];
+					if (msl + msr + oms < mults) {
+						mults= msl+msr+oms;
+					}
+				}
+			}
+			s++;
+		}
+		return memo[0][n-1];
+	}
+
+	/**
+	 * Functional solution to matrix bracketing
+	 * @locvar mmt Minimum multiplications table
+	 */
+	int funMb(int[] rs) {
+		int n= rs.length;
+		int[][] mmt= new int[n][n];
+		for (int i= 0; i < n; i++) {
+			for (int j= 0; j < n; j++) {
+				mmt[i][j]= -1;
+			}
+		}
+		return fmb(rs, mmt, 1, n-1);
+	}
+
+	/**
+	 * @return Minimum multiplications for mi*...*mj according to the table
+	 * @param mmt
+	 * and the array
+	 * @param rs Factors' ranks
+	 */
+	int fmb(int[] rs, int[][] mmt, int i, int j) {
+		int n= rs.length;
+		boolean b= j >= n || i >= n;
+		if (b) {
+			return 0;
+		}
+		if (mmt[i][j] >= 0) {
+			return mmt[i][j];
+		}
+		int s= j-i;
+		if (s <= 0) {
+			return 0;
+		}
+		int mults= Integer.MAX_VALUE;
+		for (int k= 1; k < s; k++) {
+			int msl= fmb(rs, mmt, i, i+k);
+			int msr= fmb(rs, mmt, i+k+1, i+s);
+			int oms= rs[i-1]*rs[i+k]*rs[i+s];
+			if (msl + msr + oms < mults) {
+				mults= msl+msr+oms;
+			}
+		}
+		mmt[i][j]= mults;
+		return mults;
+	}
+
+	//
+	// Main
+	//
+	
+	public static void main(String[] args) {
+		System.out.println("Aula09");
+	}
+}
